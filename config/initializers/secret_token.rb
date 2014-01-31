38ce2b001d4a0b99d941dfed6ff1607bcdf05f6d
xend <xend@xend-mbp.local>
2014-01-31 15:25:50 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsEs::Application.config.secret_key_base = '90e9b94d6235f28708c28fcd0644724c3dc122098b32b1fad52426c14fc2c4417316f2b73d29f1e47e8e834970d07b12761110f2e6f48e439b892390439e9b7a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsEs::Application.config.session_store :cookie_store, key: '_rails-es_session'
